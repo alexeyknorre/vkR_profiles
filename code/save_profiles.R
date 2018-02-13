23377e3acbed19a65ede110ccb0aa99d18bc5b2e
Alexey Knorre <a.v.knorre@gmail.com>
2018-02-13 21:52:23 +0300

    rework saving procedure once again

--- code/save_profiles.R
+++ code/save_profiles.R
@@ -6,30 +6,31 @@ options(scipen = 999)
 at <- "d75deb97e8e79b03fb86565adb1353e23e2c7dd0cb44106cdc55f05227ad0b3e64e152a685456ac4cc421"
 setAccessToken(access_token = at)
 
-# Parameters
-number <- 100000
+## Parameters #####
+number <- 10000
 
 # Set fields to get
 fields <- paste("city, country, bdate, sex, last_seen",
-                "personal, education, home_town, career",
+                "personal, career",
                 "books, music, games, interests, movies, tv",
                 sep = ", ")
 
 # Set fields to subset
-fields_wide <- c("id", "first_name", "last_name", "sex", "bdate", "university", 
-  "university_name", "faculty", "faculty_name", "graduation", "home_town", 
-  "interests", "music", "movies", "tv", "books", "games", "education_form", 
-  "education_status", "last_seen.time", "last_seen.platform", "city.id", 
-  "city.title", "country.id", "country.title", "personal.langs", 
-  "personal.political", "personal.religion", "personal.inspired_by", 
-  "personal.people_main", "personal.life_main", "personal.smoking", 
-  "personal.alcohol", "career_position")
+fields_wide <- c("id", "first_name", "last_name", "sex", "bdate", 
+                 "interests", "music", "movies", "tv", "books", "games",
+                 "city.title", "country.title", "personal.political",
+                 "career.position")
+### Functions #####
 
-get_profiles <- function(fields,
-                         n,
+get_profiles <- function(n,
+                         #fields,
+                         #fields_wide,
                          batch_size = 5000,
                          n_try = 10) {
   start_time <- Sys.time()
+  
+  #profile_fields <- fields
+  #profile_fields_wide <- fields_wide
   
   file_name <- paste0("data/","vk_",n,"profiles_", Sys.Date(),".csv")
   
@@ -38,10 +39,11 @@ get_profiles <- function(fields,
   
   # Hook if n is small
   if(n <= batch_size){
-  data <- getUsersExecute(ids, fields = fields, flatten = T, drop = T)
-  save_profiles(data, file_name, file_append = F)
+    data <- getUsersExecute(ids, fields = fields, flatten = T, drop = T)
+    #save_profiles(data, profile_fields_wide, file_name, file_append = F)
+    save_profiles(data, fields_wide, file_name, file_append = F)
   }
-
+  
   batched_ids <- split(ids,
                        ceiling(
                          seq_along(ids)/batch_size))
@@ -51,20 +53,22 @@ get_profiles <- function(fields,
   print(paste0("1/",length(batched_ids),"..."))
   
   start_ids <- as.numeric(unlist(batched_ids[1]))
-  try_again(n_try, data <- getUsersExecute(start_ids, fields = fields,flatten = T, drop = T))
+  try_again(n_try, data <- getUsersExecute(start_ids, fields,flatten = T, drop = T))
   #data <- getUsersExecute(start_ids, fields = fields,flatten = T)
   data <- as.data.table(data)
+  #save_profiles(data, profile_fields_wide, file_name, file_append = F)
   save_profiles(data, file_name, file_append = F)
   c <- 2
   
   
   for (i in batched_ids[2:length(batched_ids)]){
-      print(paste0(c,"/", length(batched_ids),"..." ))
-      try_again(n_try, d <- getUsersExecute(i, fields = fields,flatten = T, drop = T))
-      rownames(d) <- d$id
-      d <- as.data.table(d)
-      save_profiles(d, file_name)
-      c <- c + 1
+    print(paste0(c,"/", length(batched_ids),"..." ))
+    try_again(n_try, d <- getUsersExecute(i, fields = fields,flatten = T, drop = T))
+    rownames(d) <- d$id
+    d <- as.data.table(d)
+    #save_profiles(d, profile_fields_wide, file_name)
+    save_profiles(d, file_name)
+    c <- c + 1
   }
   
   # Print used time
@@ -77,18 +81,20 @@ save_profiles <- function(dataframe,
                           file_append = T) {
   
   # Fix problems with columns
-  dataframe$personal.langs <- as.character(dataframe$personal.langs)
-  dataframe$personal.langs[dataframe$personal.langs == "NULL"] <- NA
+  dataframe$personal.langs <- lapply(dataframe$personal.langs,
+                                     function(x) paste(unlist(x), collapse='|'))
   
-  dataframe$career_position <- as.character(lapply(dataframe$career,
-                                                   function(x) x$position))
-  
-  dataframe$career_position[dataframe$career_position == "NULL"] <- NA
+  dataframe$career.position <- sapply(dataframe$career,
+                                      function(x) paste0(x$position, collapse="|"))
   dataframe$career <- NULL
   
   # Subset fields to maintain structure
-  dataframe <- dataframe[,fields_wide]
-  fwrite(dataframe, file_path, append = file_append, na = "",quote = T)
+  dataframe <- dataframe[,..fields_wide]
+  fwrite(dataframe, file_path, append = file_append, na = "", quote = T)
 }
 
-get_profiles(fields, n = number)
+### Main call ######
+
+get_profiles(number)
+
+#data <- readr::read_csv("data/vk_10000profiles_2018-02-13.csv")
